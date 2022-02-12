8de78dbe2b21ef6949296aa0c6254d2e60e6963a
Martin Levesque <levesque.martin@gmail.com>
2022-02-12 13:59:04 -0500

    wip

--- http_shaping.go
+++ http_shaping.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"net/http"
 	"text/template"
+	"time"
+	"strconv"
 )
 
 // Config the plugin configuration.
@@ -27,11 +29,14 @@ type Demo struct {
 	headers  map[string]string
 	name     string
 	template *template.Template
+	loopBeginTimestamp int64
+	loopSumInBytes int64
+	loopSumOutBytes int64
 }
 
 // New created a new Demo plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	fmt.Print("Initiating http_shaping plugin \n")
+	fmt.Print("Initiating http_shaping plugin ", name, "\n")
 
 	if len(config.Headers) == 0 {
 		return nil, fmt.Errorf("headers cannot be empty")
@@ -42,30 +47,39 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 		next:     next,
 		name:     name,
 		template: template.New("demo").Delims("[[", "]]"),
+		loopBeginTimestamp: time.Now().Unix(),
+		loopSumInBytes: 0,
+		loopSumOutBytes: 0,
 	}, nil
 }
 
 func (a *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	fmt.Print("Serving http_shaping plugin \n")
 
-	for key, value := range a.headers {
-		fmt.Print("for key \n", key, "\n")
-		tmpl, err := a.template.Parse(value)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
+	fmt.Print("loopSumInBytes: ", a.loopSumInBytes, "\n")
+	fmt.Print("loopSumOutBytes: ", a.loopSumOutBytes, "\n")
+
+	requestBytesStr := req.Header.Get("content-length")
+
+	if requestBytesStr != "" {
+		requestBytes, err := strconv.ParseInt(requestBytesStr, 10, 64)
+
+		if err == nil {
+			a.loopSumInBytes = a.loopSumInBytes + requestBytes
 		}
-
-		writer := &bytes.Buffer{}
-
-		err = tmpl.Execute(writer, req)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		req.Header.Set(key, writer.String())
 	}
 
+	a.loopBeginTimestamp = time.Now().Unix()
+
 	a.next.ServeHTTP(rw, req)
+
+	responseBytesStr := rw.Header().Get("content-length")
+
+	if responseBytesStr != "" {
+		responseBytes, err := strconv.ParseInt(responseBytesStr, 10, 64)
+
+		if err == nil {
+			a.loopSumOutBytes = a.loopSumOutBytes + responseBytes
+		}
+	}
 }
