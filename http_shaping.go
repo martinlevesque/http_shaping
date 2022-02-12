0a551a64ac1c9b3440a319d85ec8ecece53ba651
Martin Levesque <levesque.martin@gmail.com>
2022-02-12 17:18:50 -0500

    wip

--- http_shaping.go
+++ http_shaping.go
@@ -9,7 +9,9 @@ import (
 	"text/template"
 	"time"
 	"strconv"
-	"github.com/martinlevesque/http_shaping/bytefmt"
+	"errors"
+	"strings"
+	"unicode"
 )
 
 // Config the plugin configuration.
@@ -38,7 +40,7 @@ type Demo struct {
 // New created a new Demo plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 	fmt.Print("Initiating http_shaping plugin ", name, "\n")
-	what := bytefmt.ToBytes("10.5GiB")
+	what := ToBytes("10.5GiB")
 	fmt.Print("what ", what, "\n")
 
 	if len(config.Headers) == 0 {
@@ -84,5 +86,61 @@ func (a *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		if err == nil {
 			a.loopSumOutBytes = a.loopSumOutBytes + responseBytes
 		}
+	}
+}
+
+
+const (
+	BYTE = 1 << (10 * iota)
+	KILOBYTE
+	MEGABYTE
+	GIGABYTE
+	TERABYTE
+	PETABYTE
+	EXABYTE
+)
+
+var invalidByteQuantityError = errors.New("byte quantity must be a positive integer with a unit of measurement like M, MB, MiB, G, GiB, or GB")
+
+// ToBytes parses a string formatted by ByteSize as bytes. Note binary-prefixed and SI prefixed units both mean a base-2 units
+// KB = K = KiB	= 1024
+// MB = M = MiB = 1024 * K
+// GB = G = GiB = 1024 * M
+// TB = T = TiB = 1024 * G
+// PB = P = PiB = 1024 * T
+// EB = E = EiB = 1024 * P
+func ToBytes(s string) (uint64, error) {
+	s = strings.TrimSpace(s)
+	s = strings.ToUpper(s)
+
+	i := strings.IndexFunc(s, unicode.IsLetter)
+
+	if i == -1 {
+		return 0, invalidByteQuantityError
+	}
+
+	bytesString, multiple := s[:i], s[i:]
+	bytes, err := strconv.ParseFloat(bytesString, 64)
+	if err != nil || bytes < 0 {
+		return 0, invalidByteQuantityError
+	}
+
+	switch multiple {
+	case "E", "EB", "EIB":
+		return uint64(bytes * EXABYTE), nil
+	case "P", "PB", "PIB":
+		return uint64(bytes * PETABYTE), nil
+	case "T", "TB", "TIB":
+		return uint64(bytes * TERABYTE), nil
+	case "G", "GB", "GIB":
+		return uint64(bytes * GIGABYTE), nil
+	case "M", "MB", "MIB":
+		return uint64(bytes * MEGABYTE), nil
+	case "K", "KB", "KIB":
+		return uint64(bytes * KILOBYTE), nil
+	case "B":
+		return uint64(bytes), nil
+	default:
+		return 0, invalidByteQuantityError
 	}
 }
